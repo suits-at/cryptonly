23ba135ff899886b36ba572dc0668a1082aebda1
suits-at <office@suits.at>
2018-11-05 19:23:47 +0100

    remove vuetify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from "vue";
-import Vuetify from "vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-
-import "vuetify/dist/vuetify.min.css";
-
-Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
