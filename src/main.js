004afa48660c1851107c6e12ad40b2f3e41f21e9
suits-at <office@suits.at>
2018-12-12 19:01:55 +0100

    update packages

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-// import "roboto-fontface/css/roboto/roboto-fontface.css";
-// import "@fortawesome/fontawesome-free/css/all.css";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.config.productionTip = false;
 
