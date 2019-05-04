4073f5ebae7476bbe4b486b82bd5221296354d97
suits-at <office@suits.at>
2019-05-04 21:48:54 +0200

    show all coins instead of top 100

--- src/lambda/callAPI.js
+++ src/lambda/callAPI.js
@@ -11,7 +11,7 @@ const requestOptions = {
 export async function handler() {
   try {
     const response = await fetch(
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5000",
       requestOptions
     );
     if (!response.ok) {
